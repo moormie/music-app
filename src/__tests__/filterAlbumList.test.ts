32c5b01a9093ba16e6329461d25977f0c440b963
Veronika Srenk <moormie@gmail.com>
2022-09-07 12:49:47 +0200

    rename filterAlbumList tests

--- src/__tests__/filterAlbumList.test.ts
+++ src/__tests__/filterAlbumList.test.ts
@@ -2,16 +2,16 @@ import { getFilteredAlbumList } from "../helpers/filterAlbumList";
 import { albumListMock } from "./mockAlbumList";
 
 describe("filtering album list", () => {
-  test("show all albums if no searched value and no selected categories", () => {
+  it("shows all albums if no searched value and no selected categories", () => {
      expect(getFilteredAlbumList("", [], albumListMock).length).toBe(3);
   });
-  test("show 2 albums if no searched value is 'to' and no selected categories", () => {
+  it("shows 2 albums if searched value is 'to' and no selected categories", () => {
      expect(getFilteredAlbumList("to", [], albumListMock).length).toBe(2);
   });
-  test("show 2 albums if no searched value and selected category is 'Soundtrack' ", () => {
+  it("shows 2 albums if no searched value and selected category is 'Soundtrack' ", () => {
      expect(getFilteredAlbumList("", ['Soundtrack'], albumListMock).length).toBe(2);
   });
-  test("show only 1 album if searched value is 'a' and selected category is 'Soundtrack' ", () => {
+  it("shows only 1 album if searched value is 'a' and selected category is 'Soundtrack' ", () => {
      expect(getFilteredAlbumList("to", ['Soundtrack'], albumListMock).length).toBe(1);
   });
 });
