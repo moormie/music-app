1ca6fba048555a5606d8f084e09e7ac217c3de54
Veronika Srenk <moormie@gmail.com>
2022-11-07 11:15:29 +0100

    github pages routing fix

--- src/index.tsx
+++ src/index.tsx
@@ -17,7 +17,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/music-app">
         <AlbumListProvider>
           <App />
         </AlbumListProvider>
