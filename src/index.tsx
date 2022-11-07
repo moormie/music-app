ab95302dcecc9795b11f1bd35b0614c104136d10
Veronika Srenk <moormie@gmail.com>
2022-11-07 11:42:40 +0100

    not a solution

--- src/index.tsx
+++ src/index.tsx
@@ -17,7 +17,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/music-app">
+      <BrowserRouter>
         <AlbumListProvider>
           <App />
         </AlbumListProvider>
